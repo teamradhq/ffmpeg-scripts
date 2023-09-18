91231c03bcf14ebe965e61c106cd75ed0135ab50
Paul Beynon <paul@beynon.app>
2023-09-18 16:19:34 +1000

    FEATURE: Test videos contain audio

--- test/functions.sh
+++ test/functions.sh
@@ -27,12 +27,12 @@ json_tag_data() {
   json_excluding_keys "$json" "${keys_to_exclude[@]}"
 }
 
-# Generate a test video file that's 30 seconds long..
+# Generate a test video file that's 30 seconds long.
 # generate_test_video black.mp4 black
 # generate_test_video white.mp4 white
 generate_test_video()
 {
   local fileName=$1
   local color=$2
-  ffmpeg -y -f lavfi -i color=c=$color:s=10x10:d=30 -c:v libx264 -g 1 -t 30 "$FIXTURE_DIR/$fileName" > /dev/null 2>&1
+  ffmpeg -y -f lavfi -i color=c=$color:s=10x10:d=30 -f lavfi -i anullsrc=r=44100:cl=stereo -c:v libx264 -g 1 -t 30 -c:a aac -shortest "$FIXTURE_DIR/$fileName" > /dev/null 2>&1
 }
